7275d8012562e43cd5fe956230f604e601d27372
kaiytr <iseungchan838@gmail.com>
2026-02-24 21:33:35 +0900

    Prefab 수정 & CinemachineCamera 생성 & Prefab Joint Anchor 위치 자동 배치

---
+++ Assets/Scripts/CPU.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class CPU : MonoBehaviour
+{
+    void Awake() { Application.targetFrameRate = 144; } // ¶Ç´Â 144
+}
---
+++ Assets/Scripts/MirrorPosition.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MirrorPosition : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log(transform.position);
+        GetComponentInChildren<HingeJoint2D>().connectedAnchor = transform.position;
+        Debug.Log(transform.position);
+    }
+}
